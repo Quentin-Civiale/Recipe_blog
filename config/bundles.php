57be31d7c2f2a037bbe17f28eb96167bde68ba6b
quentin <quentin.civiale@gmail.com>
2021-03-30 16:06:30 +0200

    Ajout du bundle lexik_jwt_authentication

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true]
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
