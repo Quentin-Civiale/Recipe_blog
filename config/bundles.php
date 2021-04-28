5190f6a77be4dcc8bab46a3bc32db0d572715fe1
quentin <quentin.civiale@gmail.com>
2021-04-28 14:59:57 +0200

    Ajout des fichiers manquants pour l'API et la mise en favoris des recettes

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
